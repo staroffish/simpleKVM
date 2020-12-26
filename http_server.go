50f7182125e362ea2f5ac4d3b46fb4c1d3707944
staroffish <34314227+staroffish@users.noreply.github.com>
2020-12-27 02:56:01 +0800

    add http api to dump pprof data

--- http_server.go
+++ http_server.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"runtime/pprof"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/staroffish/simpleKVM/capture"
@@ -35,6 +39,11 @@ func StartHttpServer(ctx context.Context, addr string, dev *capture.CaptureDevic
 	})
 
 	httpServer.GET(path, httpHandler)
+
+	httpServer.GET("/pprof", func(ctx *gin.Context) {
+		date := dumpPprof()
+		ctx.Data(http.StatusOK, "text/html", []byte(fmt.Sprintf("<html>dumpping pprof data. you can find them in the program execution path. date=%s</html>", date)))
+	})
 
 	httpServer.POST("/keydown", func(ctx *gin.Context) {
 		keyCodeStr, exists := ctx.GetQuery("key_code")
@@ -152,6 +161,50 @@ func StartHttpServer(ctx context.Context, addr string, dev *capture.CaptureDevic
 	httpServer.Static("/static", "static")
 
 	httpServer.Run(addr)
+}
+
+func dumpPprof() string {
+	workDir, err := os.Getwd()
+	nowStr := time.Now().Format("20060102150405000")
+	if err != nil {
+		log.PrintInfo("dump pprof error:Getwd:%v", err)
+		return ""
+	}
+
+	go func() {
+		fileName := fmt.Sprintf("%s_%s", "cpupprof", nowStr)
+		dumpFile, err := os.OpenFile(filepath.Join(workDir, fileName), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.PrintInfo("dump pprof error:OpenFile:%v", err)
+			return
+		}
+		defer dumpFile.Close()
+		if err := pprof.StartCPUProfile(dumpFile); err != nil {
+			log.PrintInfo("dump pprof error:StartCPUProfile:%v", err)
+			return
+		}
+		time.Sleep(time.Second * 60)
+		pprof.StopCPUProfile()
+	}()
+
+	lookFunc := func(dumpType string) {
+		fileName := fmt.Sprintf("%spprof_%s", dumpType, nowStr)
+		dumpFile, err := os.OpenFile(filepath.Join(workDir, fileName), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.PrintInfo("dump pprof error:OpenFile:%v", err)
+			return
+		}
+		defer dumpFile.Close()
+		if err := pprof.Lookup(dumpType).WriteTo(dumpFile, 1); err != nil {
+			log.PrintInfo("dump pprof error:Lookup:%v", err)
+			return
+		}
+	}
+
+	lookFunc("heap")
+	lookFunc("goroutine")
+
+	return nowStr
 }
 
 var httpTemplateFormat = `<html>
